8628dd1cce5fd8c94133409196d5bba76a178332
Abhijit Manna <abhijitmanna912001@gmail.com>
2021-07-15 17:37:31 +0530

    project completed

---
+++ src/components/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+    {
+        id: 1,
+        city: "new york",
+        img: "./img/newyork.jpeg",
+        name: "new york bridge tour",
+        info:
+            "New York City received a ninth consecutive annual record of approximately 65.2 million tourists in 2018, counting not just overnight visitors but anyone visiting for the day from over 50 miles away, including commuters.Major destinations include the Empire State Building, Ellis Island, the Statue of Liberty on Liberty Island, Broadway theatre productions, Central Park, Times Square, Coney Island, the Financial District, museums, and sports stadiums."
+    },
+    {
+        id: 2,
+        city: "paris",
+        img: "./img/paris.jpeg",
+        name: "paris lights tour",
+        info:
+            "Europe's most enchanting city, Paris is known for its many monuments, especially the Eiffel Tower, Notre-Dame Cathedral, Arc de Triomphe, Opéra Garnier, Les Invalides, etc. ... It's no wonder that the city is one of the most popular tourist destinations in the world and welcomes over 45 million visitors every year."
+    },
+    {
+        id: 3,
+        city: "london",
+        img: "./img/london.jpeg",
+        name: "london royal palace tour",
+        info:
+            "London is one of the world's leading tourism destinations, and the city is home to an array of famous tourist attractions. The city attracted 20.42 million international visitors in 2018, making it one of the world's most visited in terms of international visits.The London Eye is a giant Ferris wheel located on the edge of the River Thames. A short walk away, the area boasts the London Aquarium, Elizabeth Tower, the Houses of Parliament, Westminster Abbey and Nelson's Column. Other major tourist attractions in London include the Tower of London, Buckingham Palace , Tower Bridge Experience, Madame Tussauds, ZSL London Zoo, London Dungeon and St Paul's Cathedral."
+    },
+    {
+        id: 4,
+        city: "tokyo",
+        img: "./img/tokyo.jpeg",
+        name: "tokyo sushi tour",
+        info:
+            "Tourism in Tokyo is a major industry. In 2006, there were 420 million visits by Japanese people and 4.81 million visits by foreigners.Japanese schoolchildren on class trips typically visit Tokyo Tower. Cultural offerings in Tokyo include both omnipresent Japanese pop culture and associated districts such as Shibuya and Harajuku, subcultural attractions such as Studio Ghibli anime center. Tokyo National Museum houses over a third of the National Treasures of Japan.Other popular attractions include the Imperial Palace, Meiji Shrine, and Sensō-ji, a popular temple."
+    }
+];
